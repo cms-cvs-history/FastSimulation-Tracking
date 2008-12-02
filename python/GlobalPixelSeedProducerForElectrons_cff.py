61e10b1cff1aa0dbf05bc16651a461e44e8a9a9e
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2008-12-02 17:06:03 +0000

    Also the photon isolation needs a special track seeding! Also tune the pattern recognition emulation for KF electron tracks

--- python/GlobalPixelSeedProducerForElectrons_cff.py
+++ python/GlobalPixelSeedProducerForElectrons_cff.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 import FastSimulation.Tracking.TrajectorySeedProducer_cfi
+
 globalPixelSeedsForElectrons = FastSimulation.Tracking.TrajectorySeedProducer_cfi.trajectorySeedProducer.clone()
 globalPixelSeedsForElectrons.firstHitSubDetectorNumber = [2]
 globalPixelSeedsForElectrons.firstHitSubDetectors = [1, 2]
@@ -10,3 +11,13 @@ globalPixelSeedsForElectrons.seedingAlgo = ['GlobalPixel']
 globalPixelSeedsForElectrons.zVertexConstraint = cms.vdouble(0.5)
 globalPixelSeedsForElectrons.originRadius = cms.vdouble(0.02)
 globalPixelSeedsForElectrons.originpTMin = cms.vdouble(1.5)
+
+globalPixelSeedsForPhotons = FastSimulation.Tracking.TrajectorySeedProducer_cfi.trajectorySeedProducer.clone()
+globalPixelSeedsForPhotons.firstHitSubDetectorNumber = [2]
+globalPixelSeedsForPhotons.firstHitSubDetectors = [1, 2]
+globalPixelSeedsForPhotons.secondHitSubDetectorNumber = [2]
+globalPixelSeedsForPhotons.secondHitSubDetectors = [1, 2]
+globalPixelSeedsForPhotons.seedingAlgo = ['GlobalPixel']
+globalPixelSeedsForPhotons.zVertexConstraint = cms.vdouble(-1)
+globalPixelSeedsForPhotons.originRadius = cms.vdouble(0.02)
+globalPixelSeedsForPhotons.originpTMin = cms.vdouble(1.5)
